184d5e8810d55aedc45dffa53a78fbd3b450a658
Veive Hatake <vacyakaktyc@gmail.com>
2023-03-19 21:35:31 +0100

    feat: add alias for vite build

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import tsconfigPaths from 'vite-tsconfig-paths';
-
+import path from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [react()],
+  base: './',
+  build: {
+    outDir: './build',
+  },
   test: {
     globals: true,
     environment: 'jsdom',
@@ -13,6 +16,14 @@ export default defineConfig({
       provider: 'c8',
       reportsDirectory: './tests/coverage',
       reporter: ['text', 'json'],
+    },
+  },
+  resolve: {
+    alias: {
+      pages: path.resolve(__dirname, './src/pages'),
+      components: path.resolve(__dirname, './src/components'),
+      utils: path.resolve(__dirname, './src/utils'),
+      types: path.resolve(__dirname, './src/types'),
     },
   },
 });
