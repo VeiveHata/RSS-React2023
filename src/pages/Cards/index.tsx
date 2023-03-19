a30321fcbb8ca1fabfdb390ea1bcb0959296eba7
Veive Hatake <vacyakaktyc@gmail.com>
2023-03-19 19:24:42 +0100

    feat: add link for testing input value

--- src/pages/Cards/index.tsx
+++ src/pages/Cards/index.tsx
@@ -4,6 +4,7 @@ import { Page } from 'components/Page';
 import React from 'react';
 import { mangaList } from '../../../tests/mock.ts/cardsMock';
 import './styles.css';
+import { Link } from 'react-router-dom';
 
 class Cards extends React.Component {
   onSearch = (value: string) => {
@@ -13,6 +14,9 @@ class Cards extends React.Component {
   render() {
     return (
       <Page className="cards" name="Cards">
+        <Link className="link" to="/about">
+          Link for testing input value: About page
+        </Link>
         <SearchInput onSearch={this.onSearch} name="cardsSearch" withSave />
         <div className="cards-wrapper">
           {mangaList.map((manga) => (
